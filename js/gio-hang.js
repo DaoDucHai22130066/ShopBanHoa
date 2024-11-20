f1abfc198616cc6f7c44ed05c82c3b827725279d
nTruong257 <22130306@st.hcmuaf.edu.vn>
2024-11-20 20:30:17 +0700

    update js cho trang gio hang
    xu ly su kien nut toggle

---
+++ js/gio-hang.js
@@ -0,0 +1,9 @@
+// Xử lý sự kiện nút toggle
+document.addEventListener('DOMContentLoaded', function () {
+    var navbarToggler = document.querySelector('.navbar-toggler');
+    var navbarCollapse = document.getElementById('navbarNav');
+
+    navbarToggler.addEventListener('click', function () {
+        navbarCollapse.classList.toggle('collapse');
+    });
+});
