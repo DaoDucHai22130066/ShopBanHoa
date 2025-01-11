5ba36f80861e91b090d8c2e8afad089c8d9b32ed
Tien2403 <vantien20042004@gmail.com>
2025-01-11 20:29:17 +0700

    fix file

---
+++ ShopBanHoa/src/main/java/com/projectltw/shopbanhoa/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.projectltw.shopbanhoa.controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginController", value = "/login")
+public class controller extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
